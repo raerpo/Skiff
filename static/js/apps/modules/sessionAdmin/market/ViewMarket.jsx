0c7d0f3390b32d366e28e5861b241e2ade6b3fab
Draaakos <orlando.andaur.c@gmail.com>
2017-05-13 10:51:11 -0400

    page view supermarket now have your data and this can be seen

--- static/js/apps/modules/sessionAdmin/market/ViewMarket.jsx
+++ static/js/apps/modules/sessionAdmin/market/ViewMarket.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
 
 function getData(){
-    return data = $.get('/data/market/dfg43g3gfdg42fyy');
+    return $.get('/data/market/dfg43g3gfdg42fyy');
 }
 
+const Market = ({ comune, country, rutAdmin, totalBoxes, type }) => (
+    <div>
+        <h2>{type}</h2>
+        <div>{rutAdmin}</div>
+        <div>{comune}</div>
+        <div>{country}</div>
+        <div>{totalBoxes}</div>
+    </div>
+)
+
 class ViewMarket extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            informationMarket: {
+                market: []
+            }
+        }
+    }
+
+    componentDidMount(){
+        this.hydrateState();
+    }
+
+    hydrateState(){
+        const initialState = getData().then(data => this.setState({informationMarket: { market : data }}));
+    }
+
     render(){
+        const market = this.state.informationMarket.market.map((data, index) => 
+            <Market 
+            type={data.type}
+            rutAdmin={data.rutAdmin}
+            comune={data.comune}
+            country={data.country}
+            totalBoxes={data.totalBoxes}
+            key={index} />
+        );
+
         return(
-            <div>market</div>
+            <div>{market}</div>
         )
     }
 
